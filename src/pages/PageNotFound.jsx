f48969328c38adb10ab37e31a11829eefeac13fa
Alwi Kasim <alwikasim321@gmail.com>
2025-05-06 13:34:58 +0800

    Add nav using NavLink

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from '../components/PageNav';
+
 function PageNotFound() {
-  return <div>Page not found</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Page not Found</h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
